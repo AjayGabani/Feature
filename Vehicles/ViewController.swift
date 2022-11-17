0ef1fc1a4306a02f5155df633dd15c3a9bb69369
gabaniajay <gabaniajay@gmail.com>
2022-11-17 15:57:16 -0400

    message

---
+++ Vehicles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vehicles
+//
+//  Created by Ajay Gabani on 17/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
